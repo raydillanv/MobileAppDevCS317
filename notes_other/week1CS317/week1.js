1fe6b0e405ff237010a819a30635954ea60b28a7
Dillan Victory <raydillanv1@gmail.com>
2024-02-19 11:23:20 +0000

    First push

---
+++ notes_other/week1CS317/week1.js
@@ -0,0 +1,10 @@
+// Comment
+
+/* Long Comment
+Exercise:
+1. Declare a numeric distance constant and a boolean for metric
+2. Create a string variable of distance plus "km" or "miles" as appropriate
+3. Output result in a pop-up window
+ */
+
+console.log("Hello")
